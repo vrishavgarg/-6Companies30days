426f6e423a24098de8e5296cdd57f25b7d6133e0
vrishavgarg <gargvrishav@gmail.com>
2023-01-16 01:23:41 +0530

    day3

---
+++ adobe/3_440. K-th Smallest in Lexicographical Order.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findKthNumber(int n, int k) {
+    int ans=1;
+    for(k--;k>0;){
+        int c=0;
+        for(long long i=(long long)ans,j=i+1; i<=n; i*=10,j*=10){
+            c+=(int)(min((long long)n+1,j)-i);
+        }
+        if(k>=c){
+            ans++;
+            k-=c;
+        }else{
+            ans*=10;
+            k--;
+        }
+    }
+    return ans;
+}
+
+int main(){
+
+    int n=13,k=2;
+    cout<<findKthNumber(n,k);
+
+    return 0;
+}
